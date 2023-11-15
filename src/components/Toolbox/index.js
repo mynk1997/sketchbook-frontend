63e99b2d284e32eb930b2f70722fee1fce8d7018
Mayank <mynk1997@gmail.com>
2023-11-15 12:36:23 +0530

    Added Redux store for state management

--- src/components/Toolbox/index.js
+++ src/components/Toolbox/index.js
@@ -1,28 +1,56 @@
-import { COLORS } from "../constants"
-import styles from './index.module.css'
+import { useSelector } from "react-redux";
+import { COLORS, MENU_ITEMS } from "../constants";
+import styles from "./index.module.css";
 
-const Toolbox =()=>{
-    return(
-        <div className={styles.toolBoxContainer}>
-            <div className={styles.toolItem}>
-                <h4 className={styles.toolText}>Stroke Color</h4>
-                <div className={styles.itemContainer}>
-                    <div className={styles.colorBox} style={{backgroundColor:COLORS.BLACK}}/>
-                    <div className={styles.colorBox} style={{backgroundColor:COLORS.BLUE}}/>
-                    <div className={styles.colorBox} style={{backgroundColor:COLORS.RED}}/>
-                    <div className={styles.colorBox} style={{backgroundColor:COLORS.GREEN}}/>
-                    <div className={styles.colorBox} style={{backgroundColor:COLORS.ORANGE}}/>
-                    <div className={styles.colorBox} style={{backgroundColor:COLORS.YELLOW}}/>
-                </div>
-            </div>
-            <div className={styles.toolItem}>
-                <h4 className={styles.toolText}>Brush Size</h4>
-                <div className={styles.itemContainer}>
-                    <input type="range" min={1} max={10} step={1} onChange={()=>{}}/>
-                </div>
-            </div>
+const Toolbox = () => {
+  const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
+  const showStrokeToolOption = activeMenuItem === MENU_ITEMS.PENCIL;
+  const showBrushToolOption =
+    activeMenuItem === MENU_ITEMS.ERASER ||
+    activeMenuItem === MENU_ITEMS.PENCIL;
+  return (
+    <div className={styles.toolBoxContainer}>
+      {showStrokeToolOption && (
+        <div className={styles.toolItem}>
+          <h4 className={styles.toolText}>Stroke Color</h4>
+          <div className={styles.itemContainer}>
+            <div
+              className={styles.colorBox}
+              style={{ backgroundColor: COLORS.BLACK }}
+            />
+            <div
+              className={styles.colorBox}
+              style={{ backgroundColor: COLORS.BLUE }}
+            />
+            <div
+              className={styles.colorBox}
+              style={{ backgroundColor: COLORS.RED }}
+            />
+            <div
+              className={styles.colorBox}
+              style={{ backgroundColor: COLORS.GREEN }}
+            />
+            <div
+              className={styles.colorBox}
+              style={{ backgroundColor: COLORS.ORANGE }}
+            />
+            <div
+              className={styles.colorBox}
+              style={{ backgroundColor: COLORS.YELLOW }}
+            />
+          </div>
         </div>
-    )
-}
+      )}
+      {showBrushToolOption && (
+        <div className={styles.toolItem}>
+          <h4 className={styles.toolText}>Brush Size</h4>
+          <div className={styles.itemContainer}>
+            <input type="range" min={1} max={10} step={1} onChange={() => {}} />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default Toolbox
+export default Toolbox;
